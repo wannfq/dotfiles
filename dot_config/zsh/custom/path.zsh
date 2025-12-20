9993abe3634a90b5fb321cfc7050f2999ee709e8
Wan Afiq <wannfq@users.noreply.github.com>
2025-12-20 13:58:45 +0800

    Add opencode to path

--- dot_config/zsh/custom/path.zsh
+++ dot_config/zsh/custom/path.zsh
@@ -7,3 +7,6 @@ export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
 # Add cursor cli to PATH
 export PATH="$HOME/.local/bin:$PATH"
+
+# Add opencode to PATH
+export PATH=/home/wanxfq/.opencode/bin:$PATH
