86c9c596715301c813d329b8dce22cad9e3ff4a8
Wan Afiq <wannfq@users.noreply.github.com>
2026-01-24 22:19:19 +0800

    Autocorrect

--- dot_config/zsh/custom/configs.zsh
+++ dot_config/zsh/custom/configs.zsh
@@ -7,3 +7,5 @@ zstyle ':completion:*' menu no
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
 zstyle ':fzf-tab:complete:eza:*' fzf-preview 'eza -1 --color=always $realpath'
 zstyle ':fzf-tab:complete:bat:*' fzf-preview 'bat --color=always $realpath'
+
+setopt correct
