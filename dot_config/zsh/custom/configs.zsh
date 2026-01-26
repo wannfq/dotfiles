c916a0fb5ee2712b4fd1b7a8b3ce06680a368a55
Wan Afiq <wannfq@users.noreply.github.com>
2026-01-26 11:21:26 +0800

    Configure fzf tab completion preview

--- dot_config/zsh/custom/configs.zsh
+++ dot_config/zsh/custom/configs.zsh
@@ -4,8 +4,8 @@ zstyle ':zim:plugins:alias-finder' include-longer yes autoload yes
 # fzf tab-completions configs
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' menu no
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
-zstyle ':fzf-tab:complete:eza:*' fzf-preview 'eza -1 --color=always $realpath'
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always --icons $realpath'
+zstyle ':fzf-tab:complete:eza:*' fzf-preview 'eza -1 --color=always --icons $realpath'
 zstyle ':fzf-tab:complete:bat:*' fzf-preview 'bat --color=always $realpath'
 
 setopt correct
